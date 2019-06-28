4f96b0f9b7396aac112f974cf1317a46b93b3baf
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-06-27 17:48:25 -0700

    first commit

---
+++ button.js
@@ -0,0 +1,6 @@
+function change() {
+    var button = document.getElementById("myButton");
+    if (button.value == "Join")
+    button.textContent = "Joined!";
+    button.style.backgroundColor = "green";
+}
